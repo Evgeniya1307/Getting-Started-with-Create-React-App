f94c33f97d92ebcab24ae73e96bffd102ecb73fc
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-15 12:52:05 +0400

    export function logger(state) {     return function wrapDispatch(next)

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,11 @@
+export function logger(state) {
+    return function wrapDispatch(next) {
+      return function handleAction(action) {
+        // console.log('###state: ', state);
+        // console.log('###next: ', next);
+        // console.log('###action: ', action);
+  
+        return next(action);
+      };
+    };
+  }
