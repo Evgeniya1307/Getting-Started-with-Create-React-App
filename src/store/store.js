eba2e7713c1e513cef02d398534a91ba9e113493
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-15 13:14:40 +0400

    const rootReducer = combineReducers({ errors: errorsReducer, tasks: taskReducer });  function createStore() {   return configureStore({

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,16 @@
-i
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { logger } from './middleware/logger';
+import taskReducer from './task';
+import errorsReducer from './errors';
+
+const rootReducer = combineReducers({ errors: errorsReducer, tasks: taskReducer });
+
+function createStore() {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== 'production',
+  });
+}
+
+export default createStore;
